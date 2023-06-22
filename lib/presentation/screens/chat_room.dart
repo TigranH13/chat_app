008faa2329640407731426938925ca82c2a3d5ae
unknown <tigran.hayrapetyan17ht@gmail.com>
2023-06-22 19:35:20 +0400

    feature:send file

--- lib/presentation/screens/chat_room.dart
+++ lib/presentation/screens/chat_room.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class ChatRoom extends StatelessWidget {
   final String user;
